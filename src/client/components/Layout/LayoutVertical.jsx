4d7369ff5daa7505405f04aa8a5b5a286fba810e
Kevin Whitley <krwhitley@gmail.com>
2019-03-22 15:24:28 -0500

    released v1.4.0 - updated core styles

--- src/client/components/Layout/LayoutVertical.jsx
+++ src/client/components/Layout/LayoutVertical.jsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 
 import { Header } from './Header'
 import { Footer } from './Footer'
+import './LayoutVertical.scss'
 
 // styles
 import '../../styles/app.scss'
