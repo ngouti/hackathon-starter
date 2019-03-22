28db19d1437d91d728db8efff399a8be827ad115
Kevin Whitley <krwhitley@gmail.com>
2019-03-22 12:10:35 -0500

    released v1.3.20 - added LayoutVertical to export

--- src/client/components/Layout/LayoutVertical.jsx
+++ src/client/components/Layout/LayoutVertical.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { Router, navigate } from '@reach/router'
 import classNames from 'classnames'
 
-import { MissingPage } from '../Common/MissingPage'
 import { Header } from './Header'
 import { Footer } from './Footer'
-import { Page } from './Page'
-import Somewhere from '../Pages/Somewhere'
-import Login from '../Pages/Login'
-import Logout from '../Pages/Logout'
-import { Home } from '../Pages/Home'
-import { Content } from './Content'
 
 // styles
-import './styles/app.scss'
+import '../../styles/app.scss'
 
 export const LayoutVertical = ({ children, className, ...props }) => {
   return (
