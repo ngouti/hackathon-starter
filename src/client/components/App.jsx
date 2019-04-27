4cadab13b88a4c21e49bf8b634089fdd376114e9
Kevin Whitley <krwhitley@gmail.com>
2019-04-27 09:05:01 -0500

    removed auth from app

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
 import { Router, Link } from '@reach/router'
-import { useAuth } from '../auth'
 import styled from 'styled-components'
 import '../styles/app.scss'
 
@@ -23,33 +22,10 @@ const CenteredDiv = styled.div`
   }
 `
 
-const StyledPre = styled.pre`
-  font-size: 0.8em;
-  background-color: #eee;
-  padding: 1em;
-  max-width: 75%;
-`
-
-const LoginLogout = styled.a`
-  cursor: pointer;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`
-
 export default function App() {
-  let { user, loginAction, logoutAction, isLoggedIn } = useAuth()
-
   return (
     <CenteredDiv>
       <p>Welcome to the Arundo <em>Women's 2019 Hackathon</em></p>
-
-      {
-        isLoggedIn
-        ? <LoginLogout onClick={logoutAction}>Logout</LoginLogout>
-        : <LoginLogout onClick={loginAction}>Login</LoginLogout>
-      }
     </CenteredDiv>
   )
 }
